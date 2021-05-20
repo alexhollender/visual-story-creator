fa9d19bdee27132c6ab9f89422fef102dc29e1c7
Alex Hollender <ahollender@wikimedia.org>
2021-05-20 12:02:23 -0600

    consolidated toc.scss and creator.scss

--- public/js/creator.js
+++ public/js/creator.js
@@ -1,82 +1,132 @@
+/* global uiLang, currentPageTitle */
+
 // current text selection
-var fragment;
+let fragment;
 // current image selection
-var image;
-var keyPointsPanel = document.getElementById('addKeyPointpanel');
-var keyImagesPanel = document.getElementById('addKeyImagepanel');
-var keyPoints = [];
+let image;
+const keyPointsPanel = document.getElementById( 'addKeyPointpanel' );
+const keyImagesPanel = document.getElementById( 'addKeyImagepanel' );
+const keyPoints = [];
+const keyImages = [];
 
 function cleanupImages() {
-  $('#addKeyImagepanel').hide();
-  $('img').css('border', 'none');
+	$( '#addKeyImagepanel' ).hide();
+	$( 'img' ).css( 'border', 'none' );
 }
 
-function positionPanel(panelName, rectange) {
-  panelName.style.top = Math.floor(rectange.top + rectange.height + 10) + 'px';
-  panelName.style.left = Math.floor(rectange.left) + 'px';
-  $(panelName).show();
+function positionPanel( panelName, rectange ) {
+	panelName.style.top = Math.floor( rectange.top + rectange.height + 10 ) + 'px';
+	panelName.style.left = Math.floor( rectange.left ) + 'px';
+	$( panelName ).show();
 }
 
 // highlighting text > showing the key points panel
+// eslint-disable-next-line no-unused-vars
 function mouseUp() {
-  var textObj = window.getSelection();
-  var strLength = window.getSelection().toString().length;
-  if (strLength > 10) {
-    // as document fragment
-    fragment = textObj.getRangeAt(0).cloneContents();
-    var rect = textObj.getRangeAt(0).getBoundingClientRect();
-    positionPanel(keyPointsPanel, rect);
-    cleanupImages();
-  } else {
-    // hide both panels
-    $('#addKeyPointpanel').hide();
-    cleanupImages();
-  }
+	const textObj = window.getSelection();
+	const strLength = window.getSelection().toString().length;
+	if ( strLength > 10 ) {
+		// as document fragment
+		fragment = textObj.getRangeAt( 0 ).cloneContents();
+		const rect = textObj.getRangeAt( 0 ).getBoundingClientRect();
+		positionPanel( keyPointsPanel, rect );
+		cleanupImages();
+	} else {
+		// hide both panels
+		$( '#addKeyPointpanel' ).hide();
+		cleanupImages();
+	}
 }
 
 // clicking on an image > showing the image panel
-$('body').on('click', 'img', function(e){
-  $('img').css('border', 'none');
-  var target = e.target;
-  image = target.cloneNode();
-  $(target).css('border', '4px solid blue');
-  var imgLocation = target.getBoundingClientRect();
-  positionPanel(keyImagesPanel, imgLocation);
-  $('#addKeyPointpanel').hide();
-});
+$( 'body' ).on( 'click', 'img', function ( e ) {
+	$( 'img' ).css( 'border', 'none' );
+	const target = e.target;
+	image = target.cloneNode();
+	$( target ).css( 'border', '4px solid blue' );
+	const imgLocation = target.getBoundingClientRect();
+	positionPanel( keyImagesPanel, imgLocation );
+	$( '#addKeyPointpanel' ).hide();
+} );
 
 // adding a key point
+// eslint-disable-next-line no-unused-vars
 function addKeyPoint() {
-  var target = $("#keyPoints option:selected").val();
-  $(`#keypoint${target}`)
-    .html('')
-    .append($('<p>').append(fragment));
-  $('#addKeyPointpanel').hide();
-  keyPoints[target] = $(`#keypoint${target}`).html();
+	// eslint-disable-next-line no-jquery/no-sizzle
+	const target = $( '#keyPoints option:selected' ).val();
+	$( `#keypoint${target}` )
+		.html( '' )
+		.append( $( '<p>' ).append( fragment ) );
+	$( '#addKeyPointpanel' ).hide();
+	keyPoints[ target ] = $( `#keypoint${target}` ).html();
 }
 
 // adding a key image
+// eslint-disable-next-line no-unused-vars
 function addKeyImage() {
-  var destination = $("#keyImages option:selected").val();
-  $(`#${destination}`).html(image);
-  $('#addKeyImagepanel').hide();
-  $('img').css('border', 'none');
+	// eslint-disable-next-line no-jquery/no-sizzle
+	const destination = $( '#keyImages option:selected' ).val();
+	$( `#img${destination}` ).html( image );
+	$( '#addKeyImagepanel' ).hide();
+	$( 'img' ).css( 'border', 'none' );
+	keyImages[ destination ] = image.cloneNode();
 }
 
+function saveWikitext( wikitext ) {
+	const $form = $( '<form>' ).attr( { method: 'post', enctype: 'multipart/form-data' } ).addClass( 'oo-ui-element-hidden' );
+	const params = {
+		format: 'text/x-wiki',
+		model: 'wikitext',
+		// oldid: this.requestedRevId,
+		// wpStarttime: this.startTimeStamp,
+		// wpEdittime: this.baseTimeStamp,
+		wpTextbox1: wikitext,
+		// wpEditToken: mw.user.tokens.get( 'csrfToken' ),
+		wpUnicodeCheck: 'ℳ𝒲♥𝓊𝓃𝒾𝒸ℴ𝒹ℯ',
+		wpUltimateParam: true
+	};
+	// Add params as hidden fields
+	for ( const key in params ) {
+		$form.append( $( '<input>' ).attr( { type: 'hidden', name: key, value: params[ key ] } ) );
+	}
+	// Submit the form, mimicking a traditional edit
+	// Firefox requires the form to be attached
+	$form.attr( 'action', `https://${uiLang}.wikipedia.org/w/index.php?title=User:AHollender_(WMF)/story/${currentPageTitle}&action=submit` ).appendTo( 'body' ).trigger( 'submit' );
+}
+
+// eslint-disable-next-line no-unused-vars
 function convertToWikitext() {
-  $.post(`https://${uiLang}.wikipedia.org/api/rest_v1/transform/html/to/wikitext`, {
-    // Add empty <p> between snippets, as they may not be <p>-wrapped
-    html: keyPoints.join('<p></p>'),
-    scrub_wikitext: 1
-  }).then(function(resp) {
-    // Trim, and remove excess linbreaks
-    var wikitext = resp.trim().replace(/\n{3,}/g, '\n\n');
-    console.log(wkitext);
-  });
+	let html = '';
+	for ( let i = 0, l = Math.max( keyPoints.length, keyImages.length ); i < l; i++ ) {
+		if ( keyImages[ i ] ) {
+			const $p = $( '<div>' )
+				.addClass( 'keyImage' )
+				.append(
+					$( '<a>' )
+						.attr( 'href', image.getAttribute( 'resource' ) )
+						.append(
+							$( image ).attr( { width: 480, height: null } )
+						)
+				);
+			html += '\n' + $p[ 0 ].outerHTML;
+		}
+		if ( keyPoints[ i ] ) {
+			html += '\n' + $( '<div>' ).addClass( 'keyPoint' ).html( keyPoints[ i ] )[ 0 ].outerHTML;
+		}
+	}
+	$.post( `https://${uiLang}.wikipedia.org/api/rest_v1/transform/html/to/wikitext`, {
+		html: html,
+		// eslint-disable-next-line camelcase
+		scrub_wikitext: 1
+	} ).then( function ( resp ) {
+		// Trim, and remove excess linbreaks
+		const wikitext = resp.trim().replace( /\n{3,}/g, '\n\n' );
+		saveWikitext( wikitext );
+	} );
 }
 
 // prevent links
-$('body').on('click', 'a', function(e){
-  e.stopPropagation();
-  e.preventDefault();
-});
+$( 'body' ).on( 'click', 'a', function ( e ) {
+	e.stopPropagation();
+	e.preventDefault();
+} );
