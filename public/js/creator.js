555db3222f39ce13e1d11d30aa426be0d466eaa4
Alex Hollender <ahollender@wikimedia.org>
2021-05-19 21:22:39 -0600

    the ability to add images to the image buckets

--- public/js/creator.js
+++ public/js/creator.js
@@ -1,33 +1,47 @@
-// when the mouse comes up
-// check if there is selected text
-// if there is selected text
-// - store it in a variable
-// - get the position of the selected text
-// - show the "add excerpt" widget
-// - position the "add excerpt" widget next to the selected text
-
+// current text selection
 var text;
-var panel = document.getElementById('addKeyPointpanel');
+// current image selection
+var image;
+var keyPointsPanel = document.getElementById('addKeyPointpanel');
+var keyImagesPanel = document.getElementById('addKeyImagepanel');
 
-// showing the panel
+// highlighting text > showing the key points panel
 function mouseUp() {
     var textObj = window.getSelection();
     var textString = window.getSelection().toString().length;
-    // as document fragment
-    text = textObj.getRangeAt(0).cloneContents();
-    // as string
-    // text = window.getSelection().toString();
     if (textString > 10) {
+      // as document fragment
+      text = textObj.getRangeAt(0).cloneContents();
+      // as string
+      // text = window.getSelection().toString();
         var rect = textObj.getRangeAt(0).getBoundingClientRect();
         var top = Math.floor(rect.top + rect.height + 10) + 'px';
         var left = Math.floor(rect.left) + 'px';
-        panel.style.top = top;
-        panel.style.left = left;
+        keyPointsPanel.style.top = top;
+        keyPointsPanel.style.left = left;
         $('#addKeyPointpanel').show();
     } else {
+      // hide both panels
       $('#addKeyPointpanel').hide();
+      $('#addKeyImagepanel').hide();
+      // remove blue borders on any image that might have it
+      $('img').css('border', 'none');
     }
 }
+
+// clicking on an image > showing the image panel
+$('body').on('click', 'img', function(e){
+  $('img').css('border', 'none');
+  var target = e.target;
+  image = target.cloneNode();
+  $(target).css('border', '4px solid blue');
+  var imgLocation = target.getBoundingClientRect();
+  var top = Math.floor(imgLocation.top + imgLocation.height + 10) + 'px';
+  var left = Math.floor(imgLocation.left) + 'px';
+  keyImagesPanel.style.top = top;
+  keyImagesPanel.style.left = left;
+  $('#addKeyImagepanel').show();
+});
 
 // adding a key point
 function addKeyPoint() {
@@ -36,8 +50,16 @@ function addKeyPoint() {
   $('#addKeyPointpanel').hide();
 }
 
+// adding a key image
+function addKeyImage() {
+  var destination = $("#keyImages option:selected").val();
+  $(`#${destination}`).html(image);
+  $('#addKeyImagepanel').hide();
+  $('img').css('border', 'none');
+}
+
 // prevent links
-$('a').click(function(e) {
-    e.stopPropagation();
-    e.preventDefault();
+$('body').on('click', 'a', function(e){
+  e.stopPropagation();
+  e.preventDefault();
 });
