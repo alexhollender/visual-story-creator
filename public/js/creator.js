a86348879cefe28a398c39743aa88b7bacf1635f
Alex Hollender <ahollender@wikimedia.org>
2021-05-19 22:11:07 -0600

    two utility functions: (1) position panel, (2) cleanup images

--- public/js/creator.js
+++ public/js/creator.js
@@ -5,29 +5,33 @@ var image;
 var keyPointsPanel = document.getElementById('addKeyPointpanel');
 var keyImagesPanel = document.getElementById('addKeyImagepanel');
 
+function cleanupImages() {
+  $('#addKeyImagepanel').hide();
+  $('img').css('border', 'none');
+}
+
+function positionPanel(panelName, rectange) {
+  panelName.style.top = Math.floor(rectange.top + rectange.height + 10) + 'px';
+  panelName.style.left = Math.floor(rectange.left) + 'px';
+}
+
 // highlighting text > showing the key points panel
 function mouseUp() {
     var textObj = window.getSelection();
-    var textString = window.getSelection().toString().length;
-    if (textString > 10) {
+    var strLength = window.getSelection().toString().length;
+    if (strLength > 10) {
       // as document fragment
       text = textObj.getRangeAt(0).cloneContents();
       // as string
       // text = window.getSelection().toString();
         var rect = textObj.getRangeAt(0).getBoundingClientRect();
-        var top = Math.floor(rect.top + rect.height + 10) + 'px';
-        var left = Math.floor(rect.left) + 'px';
-        keyPointsPanel.style.top = top;
-        keyPointsPanel.style.left = left;
-        $('img').css('border', 'none');
-        $('#addKeyImagepanel').hide();
+        positionPanel(keyPointsPanel, rect);
+        cleanupImages();
         $('#addKeyPointpanel').show();
     } else {
       // hide both panels
       $('#addKeyPointpanel').hide();
-      $('#addKeyImagepanel').hide();
-      // remove blue borders on any image that might have it
-      $('img').css('border', 'none');
+      cleanupImages();
     }
 }
 
@@ -38,10 +42,7 @@ $('body').on('click', 'img', function(e){
   image = target.cloneNode();
   $(target).css('border', '4px solid blue');
   var imgLocation = target.getBoundingClientRect();
-  var top = Math.floor(imgLocation.top + imgLocation.height + 10) + 'px';
-  var left = Math.floor(imgLocation.left) + 'px';
-  keyImagesPanel.style.top = top;
-  keyImagesPanel.style.left = left;
+  positionPanel(keyImagesPanel, imgLocation);
   $('#addKeyPointpanel').hide();
   $('#addKeyImagepanel').show();
 });
